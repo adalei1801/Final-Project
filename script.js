37af19e61ed74dbfbfcfea3268f80950022e51a0
Karl Hohn <kfhohn@gmail.com>
2022-04-02 20:11:58 +0000

    added a wrapper to each page. fixed alignment of ul list items. refactored tables into ul or dl elements. added javascript to show/hide dd elements

---
+++ script.js
@@ -0,0 +1,16 @@
+const terms = document.querySelectorAll('dt');
+
+function showDef(e) {
+  let active = document.querySelector('.active');
+
+  if (active != null) {
+    active.classList.remove('active');
+  }
+
+  let def = e.target.nextElementSibling;
+  def.classList.add('active');
+}
+
+for (let i=0, ii=terms.length; i<ii; i++) {
+  terms[i].addEventListener('click', showDef);
+}
